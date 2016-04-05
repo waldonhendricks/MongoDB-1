121cd7ca5faf340bf1d4a18762fb1a3cd5c7d3b5
yang1223 <whitesheep1223@gmail.com>
2016-04-06 02:26:31 +0800

    finish query

---
+++ src/main/java/mongo/example/MongoInsert.java
@@ -0,0 +1,7 @@
+package mongo.example;
+
+/**
+ * Created by Yang Chi-Chang on 2016/4/6.
+ */
+public class MongoInsert {
+}
--- src/main/java/mongo/example/MongoQuery.java
+++ src/main/java/mongo/example/MongoQuery.java
@@ -1,16 +1,17 @@
+package mongo.example;
+
 import com.mongodb.*;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.*;
 import org.bson.Document;
 
-import java.util.Set;
-import java.util.function.Consumer;
 
 /**
  * Created by cavitation on 2016/4/5.
  */
-public class MongoConnection {
+public class MongoQuery {
 
     public static void main(String[] args) {
 
@@ -21,10 +22,26 @@ public class MongoConnection {
         MongoCollection<Document> coll = db.getCollection("users");
 
 
+        // query
+
         Document user = coll.find().first();
 
         System.out.println(user.toJson());
-        
+
+        System.out.println(coll.find(eq("Id",1)).first().toJson());
+
+        for (Document d : coll.find( and(gt("Id",3) , lt("Id",6)) )) {
+            System.out.println(d.toJson());
+        }
+
+        for (Document d : coll.find(gt("Id",7)).sort(ascending("zip"))) {
+            System.out.println(d.toJson());
+        }
+
+
+        System.out.println(coll.count());
+
+
         client.close();
 
     }
