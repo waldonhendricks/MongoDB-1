1206093008ecc27511906b0c7958261c5a73c14a
yang1223 <whitesheep1223@gmail.com>
2016-04-08 01:03:51 +0800

    delete

--- src/main/java/mongo/example/MongoDelete.java
+++ src/main/java/mongo/example/MongoDelete.java
@@ -4,6 +4,8 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.*;
+
+import com.mongodb.client.result.DeleteResult;
 import org.bson.Document;
 
 /**
@@ -13,13 +15,19 @@ public class MongoDelete {
 
     public static void main(String[] args) {
 
-        MongoClient client = new MongoClient("localhost",27017);
+        MongoClient client = new MongoClient("localhost", 27017);
 
         MongoDatabase db = client.getDatabase("test");
 
         MongoCollection<Document> collection = db.getCollection("users");
 
-//        collection.deleteOne();
+        DeleteResult result1 = collection.deleteOne(eq("Id", 14));
+
+        System.out.println(result1.getDeletedCount());
+
+        DeleteResult result2 = collection.deleteMany(gt("Id", 11));
+
+        System.out.println(result2.getDeletedCount());
 
         client.close();
 
